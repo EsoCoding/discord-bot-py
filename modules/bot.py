ee52cb87a02f3da8feff32eac8b737756374e5b2
EsoCode <arjanpoortman@gmail.com>
2023-09-17 23:53:21 +0200

    Revert "feat: Add new feature"
    This reverts commit 90d0351d697d235834a736901474c9fb16a4302b.

--- modules/bot.py
+++ modules/bot.py
@@ -12,6 +12,7 @@ from modules.logger import Logger
 
 class Bot(discord.Client):
     def __init__(self):
+<<<<<<< HEAD
         super().__init__(intents=discord.Intents.all())
         self.commands = commands.Bot(command_prefix=os.environ.get("DISCORD_BOT_PREFIX"), intents=discord.Intents.all())
 
@@ -74,3 +75,66 @@ class Bot(discord.Client):
             await ctx.send("Bot missing permissions.")
         else:
             await ctx.send("Something went wrong.")
+=======
+        super().__init__(intents=Intents.all())
+        self.commands = commands.Bot(command_prefix=os.environ.get("DISCORD_BOT_PREFIX"), intents=Intents.all())
+        self.validation = Validation()
+        self.unique_path = UniquePath()
+        self.downloader = Downloader()
+        self.zipfile = ZipFile()
+        self.uploader = Uploader()
+
+    def start(self):
+            
+        @self.commands.command(name="download", help="Download a file from a URL")
+        async def download(ctx, url: str):
+            # validate the url
+            if await self.validation.validate(ctx, url):
+                ctx.send(f"{ctx.author.mention} processing request, i'll send you a message when your haul has arrived!")
+                # generate a path with a random folder name
+                await self.unique_path.generate(ctx)
+                # download the files
+                await self.downloader.download(ctx)
+                # zip the files
+                await self.zipfile.zipfile(ctx)
+                # upload the files
+                if await self.uploader.upload_file(ctx):
+                    
+                    user = await self.commands.fetch_user(int(ctx.author.id))
+                    await self.message_user(ctx, user, f"Here is your haul {ctx.go_file_link}")
+                    
+                    Logger.info(f"sended message to {ctx.author.name} with download link: {ctx.go_file_link}")
+                    
+                # delete the temp folder
+                if await self.unique_path.delete(ctx):
+                    Logger.info("Deleted temp folder")
+
+            elif await self.validation.validate(ctx, url) == False:
+                # Log the error
+                Logger.error("Invalid URL")
+                raise commands.errors.CommandInvokeError("Invalid URL")
+            elif await url == None:
+                #log error
+                Logger.error("Missing required argument")
+                raise commands.errors.MissingRequiredArgument("Missing required argument")
+            
+        @self.commands.event
+        async def on_command_error(ctx, error):
+            if isinstance(error, commands.errors.MissingRequiredArgument):
+                await ctx.send("Missing required argument")
+            elif isinstance(error, commands.errors.CommandInvokeError):
+                await ctx.send("Invalid URL")
+            else:
+                await ctx.send(error)
+        
+
+        self.commands.run(os.environ.get("DISCORD_BOT_TOKEN"))
+
+    async def message_user(self, context, user: discord.User, message):
+        await user.send(message)
+
+
+    async def on_ready(self):
+        Logger.info(f"You are now logged in as: {self.commands.user}")
+
+>>>>>>> parent of 90d0351 (feat: Add new feature)
